e164471fdbe17992a0c8420a62ac6c45546c2d24
Md. Dipu <srabun.tangail@gmail.com>
2022-03-17 12:58:10 +0600

    receive status and take action for it

--- public/app.js
+++ public/app.js
@@ -1,9 +1,15 @@
+import checkWinner from "./actions/checkWinner.js";
 import move from "./actions/move.js";
 const cells = document.getElementsByClassName('cell');
 let player = true;
-[...cells].forEach((cell) => {
+[...cells].forEach((cell, index) => {
     cell.addEventListener('click', () => {
-        if (move(player, cell))
-            player = !player;
+        if (move(player, cell)) {
+            player = !player; // switching player
+            const checkingStatus = checkWinner([...cells], index + 1);
+            if (checkingStatus.isFindWinner) {
+                console.log(`Player ${checkingStatus.winner} is winner!`);
+            }
+        }
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import checkWinner from "./actions/checkWinner.js";
 import move from "./actions/move.js";
 
 const cells = document.getElementsByClassName('cell') as HTMLCollectionOf<HTMLDivElement>;
 
 let player: boolean = true;
 
-[...cells].forEach((cell: HTMLDivElement) => {
+[...cells].forEach((cell: HTMLDivElement, index: number) => {
     cell.addEventListener('click', () => {
-        if (move(player, cell))
-            player = !player;
+        if (move(player, cell)) {
+            player = !player; // switching player
+            const checkingStatus = checkWinner([...cells], index + 1);
+            if (checkingStatus.isFindWinner) {
+                console.log(`Player ${checkingStatus.winner} is winner!`);
+            }
+        }
     });
 });
